adb2d7e6c5cc090af631ae44876c78039c5b6197
Afonso <a70859@alunos.uminho.pt>
2019-08-28 15:41:27 +0100

    [PopularMovies][Android][RecyclerView][VWPM-0002] - Movie poster selectable implemented and condition that checks internet connectivity

--- app/src/main/java/com/example/popularmovies/ViewModels/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/ViewModels/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -26,22 +29,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        moviesRepository = MoviesRepository.getInstance();
+        if (isNetworkAvailable()) {
+            moviesRepository = MoviesRepository.getInstance();
 
-        moviesList = findViewById(R.id.moviesList);
-        moviesList.setLayoutManager(new LinearLayoutManager(this));
+            moviesList = findViewById(R.id.moviesList);
+            moviesList.setLayoutManager(new LinearLayoutManager(this));
 
-        moviesRepository.getMovies(new OnGetMoviesCallback() {
-            @Override
-            public void onSuccess(List<Movie> movies) {
-                adapter = new MoviesAdapter(movies);
-                moviesList.setAdapter(adapter);
-            }
+            moviesRepository.getMovies(new OnGetMoviesCallback() {
+                @Override
+                public void onSuccess(List<Movie> movies) {
+                    adapter = new MoviesAdapter(movies);
+                    moviesList.setAdapter(adapter);
+                }
 
-            @Override
-            public void onError() {
-                Toast.makeText(MainActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
-            }
-        });
+                @Override
+                public void onError() {
+                    Toast.makeText(MainActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else
+            Toast.makeText(this,"No internet!",Toast.LENGTH_LONG).show();
+    }
+
+    //check for Internet connectivity
+    private boolean isNetworkAvailable()
+    {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
